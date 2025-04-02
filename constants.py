ff096f6aebbe46fe7616964a360d338774f87051
Sasank5716 <sasanklama5716@gmail.com>
2025-04-02 10:33:36 +0545

    nitialize pygame and set up basic game constants

---
+++ constants.py
@@ -0,0 +1,23 @@
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GRAY = (128, 128, 128)
+COLORS = [
+    (0, 255, 255),  # I
+    (0, 0, 255),    # J
+    (255, 165, 0),  # L
+    (255, 255, 0),  # O
+    (0, 255, 0),    # S
+    (128, 0, 128),  # T
+    (255, 0, 0)     # Z
+]
+
+# Game dimensions
+BLOCK_SIZE = 30
+GRID_WIDTH = 10
+GRID_HEIGHT = 20
+SCREEN_WIDTH = BLOCK_SIZE * (GRID_WIDTH + 6)
+SCREEN_HEIGHT = BLOCK_SIZE * GRID_HEIGHT
+
+# Game settings
+FALL_SPEED = 0.5  # seconds
